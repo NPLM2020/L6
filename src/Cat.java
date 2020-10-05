044c9e4a9e13ea2d110427ad6c7a6e3a8b222c60
putilin-eo <putilin-eo@rcbi.pro>
2020-10-06 00:24:55 +0300

    Homework

---
+++ src/Cat.java
@@ -0,0 +1,8 @@
+
+public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name, 200f, -1f, 2f);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Animal cat = new Cat("Oliver");
+        Animal shepherd = new Shepherd("Sam");
+        Animal pekingese = new Pekingese("Sophie");
+
+        System.out.println(cat);
+        cat.run(300);
+        cat.swim(1);
+        cat.jump(1.5f);
+        System.out.println();
+
+        System.out.println(shepherd);
+        shepherd.run(1000);
+        shepherd.swim(5);
+        shepherd.jump(1.5f);
+        System.out.println();
+
+        System.out.println(pekingese);
+        pekingese.run(1000);
+        pekingese.swim(5);
+        pekingese.jump(1.5f);
+        System.out.println();
+
     }
 }
